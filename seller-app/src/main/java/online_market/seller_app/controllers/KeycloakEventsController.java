e80bb862faceb3bb451232785afbee2ef5cb8b19
Isaev Nikita <nikitaisaev139@gmail.com>
2025-03-18 21:51:03 +0500

    Начал настройку Keycloak

---
+++ seller-app/src/main/java/online_market/seller_app/controllers/KeycloakEventsController.java
@@ -0,0 +1,9 @@
+package online_market.seller_app.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class KeycloakEventsController {
+
+
+}
---
+++ seller-app/src/main/java/online_market/seller_app/security/SecurityBeans.java
@@ -0,0 +1,22 @@
+package online_market.seller_app.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityBeans {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(authorizeRequests ->
+                        authorizeRequests.anyRequest().authenticated())
+                .oauth2Login(Customizer.withDefaults())
+                .oauth2Client(Customizer.withDefaults())
+                .csrf(csrf -> csrf.disable())
+                .build();
+    }
+}
